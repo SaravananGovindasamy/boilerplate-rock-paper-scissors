de58982e1bc6f878540c326abf2ef798258c68c0
Saravanan Govindasamy <saravanangovindasamy.mech@gmail.com>
2024-03-03 11:17:03 +0000

    # Changes to be committed: #	modified:   RPS.py #	modified:   main.py # # Changes not staged for commit: #	modified:   RPS.py

--- RPS.py
+++ RPS.py
@@ -1,10 +1,62 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
-def player(prev_play, opponent_history=[]):
+import random
+def player(prev_play, opponent_history=[], counter=[0], current_opponent=[], kris_firstmove=[0], play_order=[{
+    "RR": 0,
+    "RP": 0,
+    "RS": 0,
+    "PR": 0,
+    "PP": 0,
+    "PS": 0,
+    "SR": 0,
+    "SP": 0,
+    "SS": 0,
+}]):
     opponent_history.append(prev_play)
+    guess = "S"
+    if opponent_history[-1] == '':
+        current_opponent.append('')
+        current_opponent[0] = ''
+        
+    
+    if len(opponent_history) >= 2:
+        if opponent_history[-2] == '' and opponent_history[-1] == 'R':
+            current_opponent[0] = "quincy_or_mrugesh"
+        elif opponent_history[-2] == '' and opponent_history[-1] == 'P':
+            current_opponent[0] = "abbey_or_kris"
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) >= 3:
+        if opponent_history[-3] == '' and opponent_history[-2] == 'P' and opponent_history[-1] == 'P':
+            current_opponent[0] = "abbey"
+        elif opponent_history[-3] == '' and opponent_history[-2] == 'P' and opponent_history[-1] == 'R':
+            current_opponent[0] = "kris"
+
+    if current_opponent[0] == "quincy_or_mrugesh":
+        last_ten = opponent_history[-1:]
+        most_frequent = max(set(last_ten), key=last_ten.count)
+
+        if most_frequent == '':
+            most_frequent = "S"
+        ideal_response = {'P': 'S', 'R': 'P', 'S': 'R'}
+        return ideal_response[most_frequent]
+
+    if current_opponent[0] == "abbey_or_kris":
+        guess = "R"
+        kris_firstmove.append(0)
+    elif current_opponent[0] == "kris":
+        if kris_firstmove[-1] == 0:
+            kris_firstmove.append(1)
+            guess = "S"
+            return guess
+        if prev_play == "P":
+            guess = "P"
+        elif prev_play == "R":
+            guess = "R"
+        else:
+            guess = "S"
+        return guess
+    elif current_opponent[0] == "abbey":
+        counter[0] += 1
+        choices = ["R", "R", "S", "S", "P"]
+        return choices[counter[0] % len(choices)]
 
     return guess
